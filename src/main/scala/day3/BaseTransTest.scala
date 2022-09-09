f98dff5ae3a099d3930d8aebc11196af7ac49dc8
chenxiaocheng <583188551@qq.com>
2022-09-09 17:14:02 +0800

    基础函数聚合

--- src/main/scala/day3/BaseTransTest.scala
+++ src/main/scala/day3/BaseTransTest.scala
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.scala._
 /**
  * 转换算子的基本用法
  * 基础函数 sum ,min,max 等
+ * 注意首先要做keyBy函数操作才能调用
  */
 object BaseTransTest {
   def main(args: Array[String]): Unit = {
@@ -32,11 +33,17 @@ object TransAggregationCaseClass {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
     val stream = env.fromElements(
-      Event("Mary", "./home", 2000L),
-      Event("Mary", "./home", 4000L),
-      Event("Bob", "./cart", 2000L),
-      Event("Bob", "./cart", 4000L)
-    ) // 使用 user 作为分组的字段，并计算最大的时间戳
+      Event("Mary", "a", 2000L),
+      Event("Mary", "b", 9000L),
+      Event("aline", "g", 3000L),
+      Event("Bob", "c", 2000L),
+      Event("Bob", "d", 4000L),
+      Event("Bob", "h", 3000L),
+      Event("Mary", "b", 11000L),
+      Event("Mary", "b", 9000L)
+    )
+    //注意这里是有限流输入的 所以每次都会输出记录
+    // 使用 user 作为分组的字段，并计算最大的时间戳
     stream.keyBy(_.user).maxBy("timestamp").print()
     env.execute()
   }
