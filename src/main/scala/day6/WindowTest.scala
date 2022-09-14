92fd6dd2a4e131bc498d56f0fa3f68557e3162c2
chenxiaocheng <583188551@qq.com>
2022-09-14 17:04:09 +0800

    滑动窗口

--- src/main/scala/day6/WindowTest.scala
+++ src/main/scala/day6/WindowTest.scala
@@ -3,7 +3,7 @@ package day6
 import day2.Event
 import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.assigners.{SlidingProcessingTimeWindows, TumblingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.assigners.{SlidingProcessingTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
 
 /**
@@ -31,17 +31,18 @@ object WindowTest {
     )
 
     //1.滚动窗口
-    //一个长度为 5 秒的滚动窗口。
     stream.keyBy(_.user)
-      .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-      .aggregate()
+      //设置一个基于时间时间的滚动窗口 长度5秒的滚动窗口
+      .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+    //设置一个基于处理时间的滚动窗口 东八区的 00-24小时
+    // .window(TumblingProcessingTimeWindows.of(Time.days(1), Time.hours(-8))
+
 
     //1.滑动处理时间窗口
     //第一个参数:表示滑动窗口的大小，
     //第二个参数:表示滑动窗口的滑动步长。
     stream.keyBy(_.user)
       .window(SlidingProcessingTimeWindows.of(Time.seconds(10), Time.seconds(5)))
-      .aggregate()
 
     //2.计数窗口
     //我们定义了一个长度为 10 的滚动计数窗口，当窗口中元素数量达到 10 的时候，就会触发 计算执行并关闭窗口。
@@ -51,7 +52,7 @@ object WindowTest {
     //2.计数窗口
     //我们定义了一个长度为 10、滑动步长为 3 的滑动计数窗口。每个窗口统计 10 个数据，每隔 3 个数据就统计输出一次结果
     stream.keyBy(_.user)
-      .countWindow(10,3)
+      .countWindow(10, 3)
 
   }
 }
